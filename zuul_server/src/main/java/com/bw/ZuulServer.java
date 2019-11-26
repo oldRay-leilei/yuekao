a1358c88e7e6affd51bea529b594f108dca5fd0f
oldRay-leilei <57738603+oldRay-leilei@users.noreply.github.com>
2019-11-26 14:10:39 +0800

    配置zuul提交

---
+++ zuul_server/src/main/java/com/bw/ZuulServer.java
@@ -0,0 +1,17 @@
+package com.bw;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServer.class,args);
+    }
+}
